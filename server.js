f6e75f74413337d531c9b36728d39655be869a27
paoladlp92 <pdlpgzz@gmail.com>
2019-03-12 21:03:31 -0600

    the third

--- server.js
+++ server.js
@@ -1,87 +1,51 @@
+// *********************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+// *********************************************************************************
+
+
+// Dependencies
+// =============================================================
 var express = require("express");
-var logger = require("morgan");
 var mongoose = require("mongoose");
 var bodyParser = require('body-parser');
 var mongojs = require("mongojs");
 
-var PORT = 3000;
-
-//which app is this anyways?
-
 // Requiring the `User` model for accessing the `users` collection
-var Entry = require("./userModel.js");
+var Entry = require("./app/models/userModel.js");
 
-// Initialize Express
-var app = express();
 var databaseUrl = "userdb";
 var collections = ["entries"];
 var db = mongojs(databaseUrl, collections);
 
-// Configure middleware
 
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Make public a static folder
-app.use(express.static("public"));
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/userdb", { useNewUrlParser: true });
 var jsonParser = bodyParser.json();
 app.use(jsonParser);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory to be served
+app.use(express.static("app/public"));
+
 // Routes
-app.get("/", function(req, res) {
-  // Create a new user using req.body
-  console.log(dbUser);
-});
+// =============================================================
+//require("app/routes/api-routes.js")(app);
 
-// Route to post our form submission to mongoDB via mongoose
+// Here we introduce HTML routing to serve different HTML files
+require("./app/routes/html-routes")(app);
+//dependencies app entry db need to work
+require("./app/routes/api-routes")(app, Entry, db);
 
-app.post("/submit", function(req, res) {
-  // Create a new user using req.body
-  console.log(req.body)
-  Entry.create(req.body)
-  //se puede llamar como sea, lo q se llamo en base de datos
-    .then(function(dbEntry) {
-      //displayt
-      //res.json(dbEntry)
-      console.log("all good" + dbEntry);
-      location.reload();
-    })
-    .catch(function(err) {
-      // If an error occurs, send the error to the client
-      res.json(err);
-    });
-});
-
-app.get("/submit2", function (req, res) {
-  // Query: In our database, go to the animals collection, then "find" everything
-  db.entries.find({}, function (err, found) {
-    // Log any errors if the server encounters one
-    if (err) {
-      console.log(err);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found.map(entries => {
-        return { id: entries._id, entry: entries.entry };
-      }));
-      //location.reload();
-    }
-  });
-});
-
-app.get("/entries", function(req, res) {
-  Entry.find({}, function(err, entries) {
-    if(err) throw err;
-    console.log("getting entries");
-    res.json(entries);
-  });
-})
-
-// Start the server
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+  console.log("App listening on PORT " + PORT);
 });
